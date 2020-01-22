e4eca4fee7e85ecd958c0237b865229a5a0e6e5b
64minato <57395252+64minato@users.noreply.github.com>
2020-01-22 13:26:22 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Rails.env == 'development'
-  (1..100).each do |i|
-      Post.create(name: "ユーザ#{i}", title: "タイトル#{i}", content: "本文#{i}")
-  end
-  Tag.create([
-    { name: '農業' },
-    { name: '畜産' },
-    { name: '肥料' },
-    { name: '飼料' },
-    { name: '苗' }
+# if Rails.env == 'development'
+#   (1..100).each do |i|
+#       Post.create(name: "ユーザ#{i}", title: "タイトル#{i}", content: "本文#{i}")
+#   end
+Tag.create([
+  { name: '農業' },
+  { name: '畜産' },
+  { name: '肥料' },
+  { name: '飼料' },
+  { name: '苗' }
   ])
-end
